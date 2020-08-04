35b784f55a5c6435470e41016baa143630b5819c
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-08-04 08:28:47 +0000

    logo image added

--- assets/js/dommanipulation.js
+++ assets/js/dommanipulation.js
@@ -1,8 +1,13 @@
+var playerName;
+
 $(document).ready(function() {
     $("#start-game").click(function() {
         $("#welcome-screen").removeClass("show");
         $("#welcome-screen").addClass("hide");
         $("#game-screen").removeClass("hide");
         $("#game-screen").addClass("show");
+        playerName = $("#player-name").val();
     })
 });
+
+console.log(playerName);
